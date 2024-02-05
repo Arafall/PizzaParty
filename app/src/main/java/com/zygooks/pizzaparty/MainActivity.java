c5bea8ad511bbe31b1e7daa3ff8ffe3482c00620
Daniel Wade <danielwade101@gmail.com>
2024-02-05 06:29:44 -0600

    Documented comments for calculateClick method in MainActivity.

--- app/src/main/java/com/zygooks/pizzaparty/MainActivity.java
+++ app/src/main/java/com/zygooks/pizzaparty/MainActivity.java
@@ -26,18 +26,31 @@ public class MainActivity extends AppCompatActivity {
         // such as layout, id, string, style, and xml.
         // setContentView method is called to add all the top level views to the activity from a layout resource.
         setContentView(R.layout.activity_main);
-        // Retrieve asset data
+        // findViewById is called to get a reference to an asset in the activity.
+        // This will link a value to the actual component and allow changes to the asset.
+        // The id is grabbed from the R class which stores the xml id index as a constant.
         mNumAttendEditText = findViewById(R.id.num_attend_edit_text);
         mNumPizzasTextView = findViewById(R.id.num_pizzas_text_view);
         //mHowHungryRadioGroup =
     }
 
+    // calculateClick is called from a button component interaction and
+    // passes the specific component that called it.
     public void calculateClick(View view) {
+        // Take the text value from the saved text box component and
+        // convert it to an integer for calculations.
         String numAttendStr = mNumAttendEditText.getText().toString();
         int numAttend = Integer.getInteger(numAttendStr);
 
+        // Calculate how many total pizzas required based on number of people, pieces for each person,
+        // and the constant for slices on each pizza.
+        // Multiply number of people with how many slices each gets rounded
+        // up to get total slices needed.
+        // Now divided by the slice number of each pizza to get total pizzas required.
         int slicesPerPerson = 4;
         int totalPizzas = (int) Math.ceil(numAttend * slicesPerPerson / (double) SLICES_PER_PIZZA);
+        // Set a text display asset to show the calculated number.
+        // Calls setText method to change text output to the view;
         mNumPizzasTextView.setText("Total pizzas: " + totalPizzas);
     }
 }

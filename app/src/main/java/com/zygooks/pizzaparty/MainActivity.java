bbf6e04b88548f9de666f9688e5605e0b95866e3
Daniel Wade <danielwade101@gmail.com>
2024-02-04 21:06:07 -0600

    Added documentation comments to onCreate method in MainActivity.java.

--- app/src/main/java/com/zygooks/pizzaparty/MainActivity.java
+++ app/src/main/java/com/zygooks/pizzaparty/MainActivity.java
@@ -13,14 +13,23 @@ public class MainActivity extends AppCompatActivity {
     private TextView mNumPizzasTextView;
     private RadioGroup mHowHungryRadioGroup;
 
+    // onCreate method is called to allow the activity to interact with the UI.
+    // savedInstanceState is the activity state before being destroyed and
+    // passed to reinstate the activity state.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        // Must be called through the super class to reach the method definition in the Activity class.
+        // An exception will occur if method is not called.
         super.onCreate(savedInstanceState);
+
+        // The R class is dynamically generated that holds constants for resource values,
+        // such as layout, id, string, style, and xml.
+        // setContentView method is called to add all the top level views to the activity from a layout resource.
         setContentView(R.layout.activity_main);
+        // Retrieve asset data
         mNumAttendEditText = findViewById(R.id.num_attend_edit_text);
         mNumPizzasTextView = findViewById(R.id.num_pizzas_text_view);
-        mHowHungryRadioGroup = 
+        //mHowHungryRadioGroup =
     }
 
     public void calculateClick(View view) {
